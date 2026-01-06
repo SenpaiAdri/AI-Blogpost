053665a8a7be12f364915e131a9803a7d14065df
SenpaiAdri <adrian31dg@gmail.com>
2026-01-06 22:00:05 +0800

    added borders on nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className="h-15 w-full absolute top-0 left-0 flex items-center justify-around">
+    <div className="h-15 w-full absolute top-0 left-0 px-50 flex items-center justify-around 
+    border border-[#6A6B70] border-dashed border-b-2 border-t-0 border-l-0 border-r-0">
       <Image
         src="/logo/ai_blogpost_text_dark.svg"
         alt="logo"
