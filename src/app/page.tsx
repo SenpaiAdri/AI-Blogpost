7246d4143f6f22a37a99601eefc3b6d0c9acd9fe
SenpaiAdri <adrian31dg@gmail.com>
2026-01-06 22:57:20 +0800

    redesigned nav bar css styling. added metadata, created structure for main content and around it.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,19 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full bg-zinc-50 font-sans dark:bg-black">
+    <div className="h-screen w-full bg-[#131316]">
       <Navbar />
+      <div className="h-full w-full flex flex-row items-center justify-center">
+        <div className="h-full hidden sm:block sm:flex-[.25] md:flex-[.5] lg:flex-1"></div>
+        {/* Main Content */}
+        <div className="h-full flex-2
+        border border-[#6A6B70] border-dashed border-b-0 border-t-0 border-x-2">
+          <main className="h-full w-full flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold">Hello World</h1>
+          </main>
+        </div>
+        <div className="h-full hidden sm:block sm:flex-[.25] md:flex-[.5] lg:flex-1"></div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,26 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className="h-15 w-full absolute top-0 left-0 px-50 flex items-center justify-around 
-    border border-[#6A6B70] border-dashed border-b-2 border-t-0 border-l-0 border-r-0">
-      <Image
-        src="/logo/ai_blogpost_text_dark.svg"
-        alt="logo"
-        width={100}
-        height={100}
-      />
-      <p className="text-white text-lg font-bold">about us</p>
+    <div className="h-18 w-full absolute top-0 left-0 flex flex-row items-center justify-center
+    border border-[#6A6B70] border-dashed border-b-2 border-t-0 border-x-0">
+      <div className="h-full hidden sm:block sm:flex-[.25] md:flex-[.5] lg:flex-1"></div>
+
+      {/* Main NavbarContent */}
+      <div className="flex-2 flex flex-row items-center justify-between px-8
+      sm:px-10 md:px-15 lg:px-20">
+        <div className="relative w-[120px] h-[130px]">
+          <Image
+            src="/logo/ai_blogpost_text_dark.svg"
+            alt="logo"
+            fill
+            className="object-contain"
+            priority
+            unoptimized
+          />
+        </div>
+        <p className="text-white text-lg font-bold">about us</p>
+      </div>
+      <div className="h-full hidden sm:block sm:flex-[.25] md:flex-[.5] lg:flex-1"></div>
     </div>
   );
 }
